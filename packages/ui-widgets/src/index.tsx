3009eaa983de4c00e42194125379471339d6abc7
Krzysztof Bielikowicz <krzysztof@causalens.com>
2023-07-27 11:32:43 +0100

    Lint fix

--- packages/ui-widgets/src/index.tsx
+++ packages/ui-widgets/src/index.tsx
@@ -1,11 +1,11 @@
 /**
-* Copyright (c) 2023 by Impulse Innovations Ltd. Part of the causaLens product.
-*
-* Use of this software is governed by the Business Source License 1.1 included in the file LICENSES/BSL.txt.
-*
-* As of the Change Date specified in that file, in accordance with the Business Source License 1.1,
-* use of this software will be governed by the Apache License, Version 2.0, included in the file LICENSES/APL.txt.
-*/
+ * Copyright (c) 2023 by Impulse Innovations Ltd. Part of the causaLens product.
+ *
+ * Use of this software is governed by the Business Source License 1.1 included in the file LICENSES/BSL.txt.
+ *
+ * As of the Change Date specified in that file, in accordance with the Business Source License 1.1,
+ * use of this software will be governed by the Apache License, Version 2.0, included in the file LICENSES/APL.txt.
+ */
 export { default as FeatureListEditor } from './feature-list-editor/feature-list-editor';
 export { default as ConfirmationModal } from './confirmation-modal/confirmation-modal';
 export type { Operation, FeatureDict } from './types';
