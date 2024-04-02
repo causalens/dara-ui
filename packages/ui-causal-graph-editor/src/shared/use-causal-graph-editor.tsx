842ce783fec5a10a8202562491aef5cd2a56cca9
Patricia Jacob <104359539+patricia-causalens@users.noreply.github.com>
2024-04-02 17:47:43 +0100

    Fix: DO-2772 edges added as undirected in editable mode (#91)
    * fix edge info issue
    
    * changelog
    
    * fix formatting
    
    * PR feedback
    
    * PR feedback2
    
    ---------
    
    Co-authored-by: Patricia Jacob <patriciajacob@Patricias-MacBook-Pro.local>

--- packages/ui-causal-graph-editor/src/shared/use-causal-graph-editor.tsx
+++ packages/ui-causal-graph-editor/src/shared/use-causal-graph-editor.tsx
@@ -21,6 +21,7 @@ import { CausalGraph, EditorMode, GraphState } from '../types';
 import { GraphActionCreators, GraphActionType, GraphReducer } from './causal-graph-store';
 import { GraphLayout, PlanarLayout } from './graph-layout';
 import { causalGraphParser } from './parsers';
+import { isDag } from './utils';
 
 export interface UseCausalGraphEditorApi {
     api: GraphApi;
@@ -52,13 +53,16 @@ export default function useCausalGraphEditor(
     const [state, dispatch] = useReducer(
         GraphReducer,
         {
-            editorMode,
             newNodesRequirePosition,
         },
         (initState: GraphState) => {
+            const parsedGraph = causalGraphParser(graphData, availableInputs);
+            const newEditorMode = editorMode ?? (isDag(parsedGraph) ? EditorMode.DEFAULT : EditorMode.PAG_VIEWER);
+
             return {
                 ...initState,
-                graph: causalGraphParser(graphData, availableInputs),
+                graph: parsedGraph,
+                editorMode: newEditorMode,
             };
         }
     );
@@ -104,8 +108,12 @@ export default function useCausalGraphEditor(
             !isEqual(lastParentData.current.nodes, graphData.nodes) ||
             !isEqual(lastParentData.current.edges, graphData.edges)
         ) {
+            const parsedGraph = causalGraphParser(graphData, availableInputs, state.graph);
+            const newEditorMode = editorMode ?? (isDag(parsedGraph) ? EditorMode.DEFAULT : EditorMode.PAG_VIEWER);
+
             dispatch({
                 graph: causalGraphParser(graphData, availableInputs, state.graph),
+                editorMode: newEditorMode,
                 type: GraphActionType.INIT_GRAPH,
             });
         }
